949e51d189d9d7a7982dd92fae5ab1b997d0cc34
isaCarvalho <isabelacarvalhocf@hotmail.com>
2020-02-23 15:07:36 -0300

    Modificacoes no login

--- js/scriptLogin.js
+++ js/scriptLogin.js
@@ -4,15 +4,15 @@ form.addEventListener("submit", function(event)
 {
     event.preventDefault();
 
-    console.log(event.target);
+    // console.log(event.target);
     let body = new FormData(event.target);
-    console.log(body);
+    // console.log(body);
 
-    fetch("http://localhost:8080/login/", {method: 'post', body})
+    fetch("http://localhost:8080/login", {method: 'post', body: body})
     .then(response => response.json())
     .then(response => {
         alert(response.message); // VER A MENSAGEM DO EMAIL INCORRETO NA API
-    
+
         if (response.message === "usuario logado com sucesso")
         {
             window.location.href = "view/inicial.html";
